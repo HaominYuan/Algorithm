b92570f0c90930a19a37d4d2ec8e2873d43cbcd8
haominyuan <tstxxy@gmail.com>
2018-11-19 03:21:27 +0800

    leetcode日常打卡 子树

--- src/leetcode/SameTree.java
+++ src/leetcode/SameTree.java
@@ -50,4 +50,17 @@ public class SameTree {
             return true;
         }
     }
+
+    static class MySolution {
+        boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == null && q == null) {
+                return true;
+            }
+            if (p == null || q == null) {
+                return false;
+            }
+            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+
 }
