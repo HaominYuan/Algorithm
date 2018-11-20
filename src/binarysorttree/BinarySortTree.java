9715f023787629a70d2ccb3659a8852f6b954e42
haominyuan <tstxxy@gmail.com>
2018-11-20 17:07:53 +0800

    二叉搜索树bug修复

--- src/binarysorttree/BinarySortTree.java
+++ src/binarysorttree/BinarySortTree.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-class BinarySortTree {
+public class BinarySortTree {
 
     private TreeNode root;
     private TreeNode pre;
@@ -16,13 +16,19 @@ class BinarySortTree {
         root = new TreeNode(0);
     }
 
+
     public BinarySortTree(TreeNode root) {
         this();
         this.root.left = root;
     }
 
+    int getSize() {
+        return root.val;
+    }
+
     void insert(int value) {
         root.left = insert(root.left, value);
+        root.val = root.val + 1;
     }
 
     private TreeNode insert(TreeNode node, int value) {
@@ -34,11 +40,10 @@ class BinarySortTree {
         } else {
             node.right = insert(node.right, value);
         }
-        root.val = root.val + 1;
         return node;
     }
 
-    List<Integer> inorderTraversal() {
+    List<Integer> inorderTraversalC() {
         List<Integer> list = new ArrayList<>();
         TreeNode p = root.left;
         Stack<TreeNode> stack = new Stack<>();
@@ -54,6 +59,24 @@ class BinarySortTree {
         }
         return list;
     }
+
+    List<Integer> inorderTraversalR() {
+        List<Integer> list = new ArrayList<>();
+        inorderTraversalR(list, root.left);
+        return list;
+    }
+
+    private void inorderTraversalR(List<Integer> list, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversalR(list, root.left);
+        list.add(root.val);
+        inorderTraversalR(list, root.right);
+    }
+
+
+
 
     int searchMax() {
         if (root.val == 0) {
@@ -106,6 +129,7 @@ class BinarySortTree {
             if (p.val == value) {
                 delete(p, direction);
                 root.val = root.val - 1;
+                return true;
             }
             pre = p;
             if (value < p.val) {
@@ -151,11 +175,13 @@ class BinarySortTree {
             rightMinPre = rightMinP;
             rightMinP = rightMinP.left;
         }
+        // 父节点
         assist(direction, rightMinP);
+        // 删除节点的左节点
         rightMinP.left = p.left;
         if (rightMinPre != null) {
+            rightMinPre.left = rightMinP.right;
             rightMinP.right = p.right;
-            rightMinPre.left = null;
         }
 
     }
--- src/binarysorttree/Test.java
+++ src/binarysorttree/Test.java
@@ -4,23 +4,30 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-
+//        758, 190, 672, 607, 884, 225, 425, 758, 514, 59
         BinarySortTree tree = new BinarySortTree();
+        System.out.println(tree.getSize());
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             list.add((int) (Math.random() * 1000));
         }
+
         for (Integer aList : list) {
             tree.insert(aList);
         }
+        System.out.println(tree.getSize());
+
         System.out.println("原始序列：" + list);
         System.out.println("序列中的最小值：" + tree.searchMin());
         System.out.println("序列中的最大值：" + tree.searchMax());
-        System.out.println("排序后的序列：" + tree.inorderTraversal());
+        System.out.println("排序后的序列：" + tree.inorderTraversalC());
+        System.out.println("序列大小：" + tree.getSize());
+
         for (Integer aList : list) {
             System.out.println("删除的元素是：" + aList);
             tree.delete(aList);
-            System.out.println("删除后的序列：" + tree.inorderTraversal());
+            System.out.println("删除后的序列：" + tree.inorderTraversalC());
+            System.out.println("序列大小：" + tree.getSize());
         }
     }
 
