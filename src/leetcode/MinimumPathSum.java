0f6392e009b1c5bd9f38259700a5e1b21edbdad1
haominyuan <tstxxy@gmail.com>
2018-11-26 00:35:41 +0800

    leetcode 最小路径修改

--- src/leetcode/MinimumPathSum.java
+++ src/leetcode/MinimumPathSum.java
@@ -15,20 +15,21 @@ public class MinimumPathSum {
 
     static class Solution {
         public int minPathSum(int[][] grid) {
-            for (int i = grid.length - 2; i >= 0; i--) {
-                grid[i][grid[i].length - 1] = grid[i][grid[i].length - 1] + grid[i + 1][grid[i].length - 1];
+            if (grid.length == 0) {
+                return 0;
             }
-            for (int j = grid[0].length - 2; j >= 0; j--) {
-                grid[grid.length - 1][j] = grid[grid.length - 1][j] + grid[grid.length - 1][j + 1];
+            for (int j = 1; j < grid[0].length; j++) {
+                grid[0][j] = grid[0][j - 1] + grid[0][j];
             }
-
-            for (int i = grid.length - 2; i >= 0; i--) {
-                for (int j = grid[i].length - 2; j >= 0; j--) {
-                    grid[i][j] = grid[i][j] + Math.min(grid[i + 1][j], grid[i][j + 1]);
+            for (int i = 1; i < grid.length; i++) {
+                grid[i][0] = grid[i - 1][0] + grid[i][0];
+            }
+            for (int i = 1; i < grid.length; i++) {
+                for (int j = 1; j < grid[0].length; j++) {
+                    grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
                 }
             }
-
-            return grid[0][0];
+            return grid[grid.length - 1][grid[0].length - 1];
         }
     }
 }
