a1f6c4cb1cc3b0a1bed1a993de18c90c31b18cd4
haominyuan <tstxxy@gmail.com>
2018-11-27 01:31:49 +0800

    leetcode&剑指offer 只出现一个数字II

--- src/leetcode/SingleNumberII.java
+++ src/leetcode/SingleNumberII.java
@@ -5,26 +5,33 @@ public class SingleNumberII {
         System.out.println(new Solution().singleNumber(new int[]{2, 2, 2, 3, 4, 4, 4}));
     }
 
+    static class NaiveSolution {
+        public int singleNumber(int[] nums) {
+            int result = 0, sum = 0, s = 1;
+            for (int i = 0; i < 32; i++) {
+                for (int num : nums) {
+                    if ((num & s) != 0) {
+                        sum = sum + 1;
+                    }
+                }
+                if (sum % 3 != 0) {
+                    result = result | s;
+                }
+                s = s << 1;
+                sum = 0;
+            }
+            return result;
+        }
+    }
+
     static class Solution {
         public int singleNumber(int[] nums) {
-            int test = 0;
-            for (int i = 0; i < nums.length; i++) {
-                test = test ^ nums[i];
+            int a = 0, b = 0;
+            for (int c : nums) {
+                b = b ^ c & ~a;
+                a = a ^ c & ~b;
             }
-
-            int test1 = 0;
-            for (int i = 0; i < nums.length; i++) {
-                test1 = test1 ^ (test ^ nums[i]);
-            }
-
-//            if ((((nums.length - 1) / 3) % 2) == 0) {
-//                System.out.println("偶数");
-//                return test1 ^ test;
-//            } else {
-//                System.out.println("奇数");
-//                return test1;
-//            }
-            return test1;
+            return b;
         }
     }
 }
