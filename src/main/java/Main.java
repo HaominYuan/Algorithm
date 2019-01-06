f437cf8c59303fbc37f4c963ba1aaa264fdc9b0f
haominyuan <tstxxy@gmail.com>
2019-01-06 14:08:54 +0800

    程序员代码面试指南 构造数组的MaxTree leetcode 最大二叉树

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,51 @@
-//import java.io.IOException;
-//
-//public class Main {
-//
-//    public static void main(String[] args) throws IOException, InterruptedException {
-//        int[] array = new int[]{};
-//        array.length
-//    }
-//}
-//
-//
-//
+import leetcode.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[]{3, 4, 5, 1, 2};
+        HashMap<TreeNode, TreeNode> map = getLeftBigMap(array);
+        for (Map.Entry<TreeNode, TreeNode> entry : map.entrySet()) {
+            System.out.print(entry.getKey().val + " -> ");
+            if (entry.getValue() == null) {
+                System.out.println("null");
+            } else {
+                System.out.println(+entry.getValue().val);
+            }
+        }
+    }
+
+    private static HashMap<TreeNode, TreeNode> getLeftBigMap(int[] array) {
+        HashMap<TreeNode, TreeNode> map = new HashMap<>();
+        Stack<TreeNode> stack = new Stack<>();
+        for (int i1 : array) {
+            while (!stack.isEmpty() && stack.peek().val < i1) {
+                popAndSetMap(stack, map);
+            }
+            stack.push(new TreeNode(i1));
+        }
+        while (!stack.isEmpty()) {
+            popAndSetMap(stack, map);
+        }
+        return map;
+    }
+
+
+    private static void popAndSetMap(Stack<TreeNode> stack, HashMap<TreeNode, TreeNode> map) {
+        // 如果栈是空的，抛出异常
+        if (stack.isEmpty()) {
+            throw new IllegalArgumentException("Stack is empty!");
+        }
+        TreeNode temp = stack.pop();
+        if (stack.isEmpty()) {
+            map.put(temp, null);
+        } else {
+            map.put(temp, stack.peek());
+        }
+    }
+
+
+}
--- src/main/java/leetcode/MaximumBinaryTree.java
+++ src/main/java/leetcode/MaximumBinaryTree.java
@@ -1,48 +1,110 @@
-//package leetcode;
-//
-//public class MaximumBinaryTree {
-//    public static void main(String[] args) {
-//        System.out.println(new NaiveSolution().constructMaximumBinaryTree(new int[]{3, 2, 1, 6, 0, 5}));
-//    }
-//
-//
-//    static class NaiveSolution {
-//        public TreeNode constructMaximumBinaryTree(int[] nums) {
-//            if (nums == null || nums.length == 0) {
-//                return null;
-//            }
-//            return func(nums, 0, nums.length - 1);
-//        }
-//
-//        TreeNode func(int[] nums, int left, int right) {
-//            if (left == right) {
-//                return new TreeNode(nums[left]);
-//            }
-//
-//            int mid = (left + right) / 2;
-//            TreeNode leftNode = func(nums, left, mid);
-//            TreeNode rightNode = func(nums, mid + 1, right);
-//            return insert(leftNode, rightNode);
-//        }
-//
-//        TreeNode insert(TreeNode leftNode, TreeNode rightNode) {
-//            if (leftNode == null && rightNode == null) {
-//                return null;
-//            }
-//            if (leftNode == null) {
-//                return rightNode;
-//            }
-//            if (rightNode == null) {
-//                return leftNode;
-//            }
-//            // 假定左子树比较大
-//            if (leftNode.val < rightNode.val) {
-//                TreeNode temp = leftNode;
-//                leftNode = rightNode;
-//                rightNode = temp;
-//            }
-//
-//
-//        }
-//    }
-//}
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class MaximumBinaryTree {
+    public static void main(String[] args) {
+        TreeNode treeNode = new Solution().constructMaximumBinaryTree(new int[]{3, 2, 1, 6, 0, 5});
+//        System.out.println(new Solution().constructMaximumBinaryTree(new int[]{3, 2, 1, 6, 0, 5}));
+        System.out.println(treeNode);
+    }
+
+
+
+    static class MySolution {
+        public TreeNode constructMaximumBinaryTree(int[] nums) {
+            return buildTree(nums, 0, nums.length - 1);
+        }
+
+        TreeNode buildTree(int[] nums, int left, int right) {
+            if (left > right) {
+                return null;
+            }
+
+            if (left == right) {
+                return new TreeNode(nums[left]);
+            }
+
+            int maxIndex = left;
+            for (int i = left + 1; i <= right; i++) {
+                if (nums[maxIndex] < nums[i]) {
+                    maxIndex = i;
+                }
+            }
+            TreeNode treeNode = new TreeNode(nums[maxIndex]);
+            treeNode.left = buildTree(nums, left, maxIndex - 1);
+            treeNode.right = buildTree(nums, maxIndex + 1, right);
+            return treeNode;
+        }
+    }
+
+//    [6, 3, 2, 1, 5, 0]
+    static class Solution {
+        public TreeNode constructMaximumBinaryTree(int[] arr) {
+            TreeNode[] nArr = new TreeNode[arr.length];
+            for (int i = 0; i != arr.length; i++) {
+                nArr[i] = new TreeNode(arr[i]);
+            }
+            Stack<TreeNode> stack = new Stack<>();
+
+            // left
+            HashMap<TreeNode, TreeNode> lBigMap = new HashMap<>();
+            for (int i = 0; i != nArr.length; i++) {
+                TreeNode curTreeNode = nArr[i];
+                while (!stack.isEmpty() && stack.peek().val < curTreeNode.val) {
+                    popStackSetMap(stack, lBigMap);
+                }
+                stack.push(curTreeNode);
+            }
+            while (!stack.isEmpty()) {
+                popStackSetMap(stack, lBigMap);
+            }
+
+            // right
+            // stack内一定没有元素，因为从上面循环出来就一定是空
+            HashMap<TreeNode, TreeNode> rBigMap = new HashMap<>();
+            for (int i = nArr.length - 1; i != -1; i--) {
+                TreeNode curTreeNode = nArr[i];
+                while (!stack.isEmpty() && stack.peek().val < curTreeNode.val) {
+                    popStackSetMap(stack, rBigMap);
+                }
+                stack.push(curTreeNode);
+            }
+            while (!stack.isEmpty()) {
+                popStackSetMap(stack, rBigMap);
+            }
+
+            TreeNode head = null;
+            for (int i = 0; i != arr.length; i++) {
+                TreeNode curNode = nArr[i];
+                TreeNode left = lBigMap.get(curNode);
+                TreeNode right = rBigMap.get(curNode);
+                if (left == null && right == null) {
+                    head = curNode;
+                } else if (left == null) {
+                    right.left = curNode;
+                } else if (right == null) {
+                    left.right = curNode;
+                } else {
+                    if (left.val < right.val) {
+                        left.right = curNode;
+                    } else {
+                        right.left = curNode;
+                    }
+                }
+            }
+            return head;
+        }
+
+        private void popStackSetMap(Stack<TreeNode> stack, HashMap<TreeNode, TreeNode> map) {
+            TreeNode popNode = stack.pop();
+            if (stack.isEmpty()) {
+                map.put(popNode, null);
+            } else {
+                map.put(popNode, stack.peek());
+            }
+        }
+    }
+
+}
